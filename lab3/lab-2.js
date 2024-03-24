0778851444f440766aa9090154c6a4f993f29ff6
GimbiyaSa <gimbiyas@gmail.com>
2024-03-24 19:16:33 +0200

    refactored lab3 code

--- lab3/lab-2.js
+++ lab3/lab-2.js
@@ -1,5 +1,21 @@
 var books = [{title: "The Great Gatsby", author: "F. Scott Fitzgerald", year:"1925"},
             {title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960},
-            {title: "1984", author: "George Orwell", year: 1949}]
+            {title: "1984", author: "George Orwell", year: 1949},
+            {title: "We are the world", author: "George Orwell", year: 1949}]
 
-console.log(books[0].title)
+function getBookTitlesByYear(books){
+    var booksList = [];
+    for(i=0; i<books.length; i++){
+        let key = books[i].year;
+        for(j=0; j<books.length; j++){
+            if(key == books[j].year){
+                booksList.push(books[j].title);
+            }
+        }
+        console.log(key + ":" + booksList);
+        booksList = [];
+    }
+        
+}
+
+getBookTitlesByYear(books);
