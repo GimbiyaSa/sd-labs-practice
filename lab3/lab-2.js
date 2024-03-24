2b28f822201a05b2284fa2ccc41a3405739ca38f
GimbiyaSa <gimbiyas@gmail.com>
2024-03-24 18:48:09 +0200

    first commit

---
+++ lab3/lab-2.js
@@ -0,0 +1,5 @@
+var books = [{title: "The Great Gatsby", author: "F. Scott Fitzgerald", year:"1925"},
+            {title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960},
+            {title: "1984", author: "George Orwell", year: 1949}]
+
+console.log(books[0].title)
