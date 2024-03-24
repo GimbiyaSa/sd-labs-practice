94e236137f89ce76e349479c8a24efaaaf5c9587
GimbiyaSa <gimbiyas@gmail.com>
2024-03-24 14:34:55 +0200

    first commit

--- 1148252-lab-four.js
+++ 1148252-lab-four.js
@@ -1,17 +1,26 @@
+// Check if document is defined and the button element exists
+if (typeof document !== 'undefined') {
+  const myButton = document.getElementById('submitButton');
 
-const myButton = document.getElementById('submitButton');
-const input = document.getElementById('Country').value;
+  if (myButton) {
+    // Adding a click event listener
+    myButton.addEventListener('click', function() {
+      const input = document.getElementById('Country').value;
+      getCountries(input);
+    });
 
-if (myButton && input){
-  // Adding a click event listener
-  myButton.addEventListener('click', function() {
-    getCountries(input);
-  });
-
-  async function getCountries(Country) {
-  const response = await fetch('https://restcountries.com/v3.1/name/'+Country+'');
-  const countries = await response.json();
-  console.log(countries);
-}
-
+    async function getCountries(Country) {
+      try {
+        const response = await fetch(`https://restcountries.com/v3.1/name/${Country}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const countries = await response.json();
+        console.log(countries);
+      } catch (error) {
+        console.error('Error fetching data:', error.message);
+      }
+    }
+    
   }
+}
